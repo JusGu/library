a4c54d3ddb86a2d057fdceb6be6acb4403625a9b
Justin Gu <juustin.gu@gmail.com>
2022-06-13 20:57:46 -0400

    Added css layout and modal

---
+++ script.js
@@ -0,0 +1,17 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+// Book.prototype.info = function(){
+//     read = this.read ? "read" : "not read yet";
+//     return(this.title + " by " + this.author + ", " + this.pages + " pages, " + read);
+// }
+// hello = new Book("The Hobbit","Hello","Hello","true");
+// console.log(hello.info());
+function addBookToLibrary(myBook) {
+  myLibrary.push(myBook);
+}
